b90320959cda08cd23af1f97aa016f865c6cbe64
Guillaume Walck <gwalck@techfak.uni-bielefeld.de>
2022-11-06 18:33:25 +0100

    Workaround for firmware not sending checksum for `unknown command` message (#4)
    Any Teensy/Arduino firmware using SerialProtocol library prior to 23-08-2021 did not add a checksum to the `unknown cmd` message.
    This bug was corrected on 23.8.21 (rev 47 of https://svn.techfak.uni-bielefeld.de/citec/projects/AGNIserialProtocol/src/teensy/SerialProtocol).
    However, there might be other firmware not sending the checksum too.
    As some new commands were introduced recently (159268e and 829a64e), this firmware will reply with an invalid message causing the serial protocol driver to throw an exception.
    
    To gracefully handle such buggy firmware, this PR changes the serial protocol driver to ignore a missing checksum in conjuction with an `unknown cmd` message.
    A warning is printed, pointing out the missing checksum and suggesting to fix the firmware.

--- libio/serial_protocol.cpp
+++ libio/serial_protocol.cpp
@@ -1422,11 +1422,18 @@ void SerialProtocolBase::read_error(uint8_t* buf)
           std::cerr << " device encountered an undocumented error" << std::endl;
         }
     }
+    return;
   }
-  else
+
+  // HANDLE bug in an old firmware version (prior to 23/08/2021) that don't send a checksum for SP_ERR_UNKNCMD
+  if (error_len == SP_ERR_TYP_LEN && buf[SP_ERR_TYP_OFFSET] == SP_ERR_UNKNCMD)
   {
-    throw std::runtime_error(std::string("sp:err, no error info"));
+    // ignore checksum, and fake that the read was successful
+    std::cerr << " device says unknown command, but didn't send a checksum. Consider updating the firmware to fix this bug." << std::endl;
+    return;
   }
+
+  throw std::runtime_error(std::string("sp:err, invalid error message"));
 }
 
 void SerialProtocolBase::read(bool local_throw_at_timeout)
